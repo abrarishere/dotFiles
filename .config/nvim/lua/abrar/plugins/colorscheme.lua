02e6738820fadf52a2cef56fff7f69185d63227d
Muhammad Abrar <legendabrar44@gmail.com>
2024-04-30 20:37:31 +0500

    colorizer and colorutil in nvim

--- .config/nvim/lua/abrar/plugins/colorscheme.lua
+++ .config/nvim/lua/abrar/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 return {
-  "folke/tokyonight.nvim",
+  "dasupradyumna/midnight.nvim",
   priority = 1000,
   config = function()
     local bg = "#011628"
@@ -12,32 +12,32 @@ return {
     local fg_gutter = "#627E97"
     local border = "#547998"
 
-    require("tokyonight").setup({
-      style = "night",
-      transparent = true,
-    styles = {
-       sidebars = "transparent",
-       floats = "transparent",
-    },
-      on_colors = function(colors)
-        colors.bg = bg
-        colors.bg_dark = bg_dark
-        colors.bg_float = bg_dark
-        colors.bg_highlight = bg_highlight
-        colors.bg_popup = bg_dark
-        colors.bg_search = bg_search
-        colors.bg_sidebar = bg_dark
-        colors.bg_statusline = bg_dark
-        colors.bg_visual = bg_visual
-        colors.border = border
-        colors.fg = fg
-        colors.fg_dark = fg_dark
-        colors.fg_float = fg
-        colors.fg_gutter = fg_gutter
-        colors.fg_sidebar = fg_dark
-      end
-    })
+    -- require("tokyonight").setup({
+    --   style = "night",
+    --   transparent = true,
+    -- styles = {
+    --    sidebars = "transparent",
+    --    floats = "transparent",
+    -- },
+    --   on_colors = function(colors)
+    --     colors.bg = bg
+    --     colors.bg_dark = bg_dark
+    --     colors.bg_float = bg_dark
+    --     colors.bg_highlight = bg_highlight
+    --     colors.bg_popup = bg_dark
+    --     colors.bg_search = bg_search
+    --     colors.bg_sidebar = bg_dark
+    --     colors.bg_statusline = bg_dark
+    --     colors.bg_visual = bg_visual
+    --     colors.border = border
+    --     colors.fg = fg
+    --     colors.fg_dark = fg_dark
+    --     colors.fg_float = fg
+    --     colors.fg_gutter = fg_gutter
+    --     colors.fg_sidebar = fg_dark
+    --   end
+    -- })
 
-    vim.cmd("colorscheme tokyonight")
+    vim.cmd("colorscheme midnight")
   end
 }
