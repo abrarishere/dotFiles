799732f9f0262eb721fa0cc28955910b9e6228a6
Muhammad Abrar <legendabrar44@gmail.com>
2024-07-03 22:37:05 +0500

    See comkt in detaisl

--- nvim/.config/nvim/lua/abrar/core/themery.lua
+++ nvim/.config/nvim/lua/abrar/core/themery.lua
@@ -1,14 +1,24 @@
 
   -- Themery block
 -- This block will be replaced by Themery.
-local rose_pine = require('rose-pine')
-rose_pine.setup({
-variant = "main",
+local catppuccin = require('catppuccin')
+catppuccin.setup({
+flavour = "macchiato",
+transparent_background = false,
+show_end_of_buffer = false,
+term_colors = false,
 styles = {
-italic = false,
+comments = { 'bold' },
+conditionals = { 'bold' },
+},
+integrations = {
+cmp = true,
+gitsigns = true,
+nvimtree = true,
+treesitter = true,
 },
 })
 
-vim.cmd("colorscheme rose-pine-main")
-vim.g.theme_id = 4
+vim.cmd("colorscheme catppuccin")
+vim.g.theme_id = 11
 -- end themery block
